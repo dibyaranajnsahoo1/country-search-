8feaa410071a76ed598005b60832f8a7b1d25c8a
Dibya Ranjan sahoo <ranjandibya34512@gmail.com>
2024-12-07 13:26:25 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
-import Flag from './components/Flags'
+import CountrySearch from './components/CountrySearch'
 
 function App() {
   return (
     <div className="App">
-     <Flag/>
+      <CountrySearch />
     </div>
   );
 }
--- src/components/FlagItems.jsx
+++ src/components/FlagItems.jsx
@@ -1,31 +1,30 @@
 import React from "react";
 
 const FlagItems = ({ img, name }) => {
-    return (
-        <div
-            className="countryCard"
-            style={{
-                border: "1px solid #ccc",
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                justifyContent: "center",
-                height: "180px",
-                width: "180px",
-                padding: "10px",
-                textAlign: "center",
-                borderRadius: "8px",
-                boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
-            }}
-        >
-            <img
-                src={img}
-                alt={`Flag of ${name}`}
-                style={{ width: "100px", height: "60px", objectFit: "cover" }}
-            />
-            <h2 style={{ fontSize: "16px", margin: "10px 0 0" }}>{name}</h2>
-        </div>
-    );
+  return (
+    <div
+      className="countryCard"
+      style={{
+        border: "1px solid #ccc",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "180px",
+        width: "180px",
+        padding: "10px",
+        textAlign: "center",
+        borderRadius: "8px",
+      }}
+    >
+      <img
+        src={img}
+        alt={`Flag of ${name}`}
+        style={{ width: "100px", height: "60px" }}
+      />
+      <h2 style={{ fontSize: "16px", margin: "10px 0" }}>{name}</h2>
+    </div>
+  );
 };
 
 export default FlagItems;
